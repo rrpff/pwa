79384296018919a30922cef69162abba88199435
Richard Foster <richard@madetech.co.uk>
2017-11-12 14:27:01 +0000

    Fix history error

--- app/link.js
+++ app/link.js
@@ -7,11 +7,7 @@ class Link extends React.Component {
 
     const { location, push, replace } = this.context.router
 
-    if (location.pathname === window.location.pathname) {
-      replace(this.props.href)
-    } else {
-      push(this.props.href)
-    }
+    push(this.props.href)
   }
 
   render () {
